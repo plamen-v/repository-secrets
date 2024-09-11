1a166cbbfc8be9184164689d71cb939916611791
Plamen Vasilev <plamen.v2.1.0@gmail.com>
2024-09-11 12:32:00 +0300

    Setup - Small fixes.

--- src/main/java/org/lab/secrets/SecretsApplication.java
+++ src/main/java/org/lab/secrets/SecretsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecretsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RepositorySecretsApplication.class, args);
+		SpringApplication.run(SecretsApplication.class, args);
 	}
 
 }
--- src/test/java/org/lab/secrets/SecretsApplicationTests.java
+++ src/test/java/org/lab/secrets/SecretsApplicationTests.java
@@ -1,4 +1,4 @@
-package org.plamen.repository_secrets;
+package org.lab.secrets;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SecretsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }

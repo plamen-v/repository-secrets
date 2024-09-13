ce1159c0ae6dcfa6c42f7a07a098843b34b0309b
Plamen Vasilev <plamen.v2.1.0@gmail.com>
2024-09-13 09:27:28 +0300

    Working on UI

--- app/src/main/java/org/lab/secrets/AppConfiguration.java
+++ app/src/main/java/org/lab/secrets/AppConfiguration.java
@@ -6,8 +6,38 @@ import org.lab.secrets.repository.RecordRepository;
 import org.lab.secrets.service.RecordService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfiguration {
+@EnableWebSecurity
+public class AppConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+
+    @Bean
+    public SecurityFilterChain build(HttpSecurity http) throws Exception {
+        http.sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/**").permitAll();
+                    authorize.anyRequest().authenticated();
+
+                })
+                .formLogin((formLogin) -> {
+                    formLogin.disable();
+                });
+
+        return http.build();
+    }
+
 
 }
--- app/src/main/resources/static/js/app.js
+++ app/src/main/resources/static/js/app.js
@@ -1,1 +1,38 @@
-alert(1999);
+$( document ).ready(function() {
+
+
+    $.ajax({
+        url: "http://localhost:8081/api/records",
+        type: 'GET',
+       // headers: { 'Access-Control-Allow-Origin': '*' },
+        success: function(res) {
+           console.log(res);
+           addRecords(res);
+        }
+    });
+});
+
+
+function addRecords(records){
+    console.log("in");
+     let tBody = $('#tblRecords > tbody');
+     let rows = "";
+
+     $.each(records, function( index, record) {
+        rows += generateRecordRow(record);
+        $.each(record.secrets, function(j, secret){
+            rows += generateSecretRow(secret);
+        });
+     });
+
+
+     tBody.html(rows);
+}
+
+function generateRecordRow(record){
+    return "<tr><td>" + record.url + "</td><td><a>add secret</a> <a>edit</a> <a>elete</a></td></tr>";
+}
+
+function generateSecretRow(secret){
+    return "<tr><td>" + secret + "</td><td><a>add secret</a> <a>edit</a> <a>elete</a></td></tr>";
+}
